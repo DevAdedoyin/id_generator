9225efcc2d2658de93722a6a5ca994fd255a0a60
Adedoyin <isradeoluwa@gmail.com>
2020-06-12 12:29:02 +0100

    ID Generator UI Completed

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: IdGenerator(),));
+
+class IdGenerator extends StatefulWidget {
+  @override
+  _IdGeneratorState createState() => _IdGeneratorState();
+}
+
+class _IdGeneratorState extends State<IdGenerator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(title: Text("Generate ID", style: TextStyle(fontWeight: FontWeight.bold),), backgroundColor: Colors.grey[800],
+        centerTitle: true, elevation: 0,),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+          Center(child: CircleAvatar(backgroundImage: AssetImage("assets/bines.jpg"), radius: 40,),),
+          SizedBox(height: 10.0),
+          Divider(height: 10, color: Colors.red,),
+          SizedBox(height: 10.0,),
+          Text("Name", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, letterSpacing: 1, fontSize: 20),),
+          Text("Adedoyin", style: TextStyle(color: Colors.yellowAccent, fontWeight: FontWeight.bold, letterSpacing: 1, fontSize: 30),),
+          SizedBox(height: 10.0,),
+          Text("ID", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, letterSpacing: 1, fontSize: 20),),
+          Text("355545454", style: TextStyle(color: Colors.yellowAccent, fontWeight: FontWeight.bold, letterSpacing: 1, fontSize: 30),),
+          SizedBox(height: 15.0,),
+          Row(children: <Widget>[
+            Icon(Icons.mail, color: Colors.white,),
+            Text("dreezybines@gmail.com", style: TextStyle(color: Colors.amber[400], letterSpacing: 1, fontSize: 30),),
+          ],)
+        ],),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: (){}, child: Icon(Icons.shuffle),),
+    );
+  }
+}
+
